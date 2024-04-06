685ca98e0e0641d71f465a3999b25b5107aa554a
maciuszek <szczecinska.duma@gmail.com>
2024-04-06 01:49:33 -0400

    prototype lfu-like word count sorting

--- src/main/java/com/maciuszek/wordcount/service/CountService.java
+++ src/main/java/com/maciuszek/wordcount/service/CountService.java
@@ -1,26 +1,83 @@
 package com.maciuszek.wordcount.service;
 
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class CountService {
 
+    @NoArgsConstructor
+    private static class CountNode {
+
+        CountNode next;
+        CountNode prev;
+
+        String data;
+        int frequency;
+
+        CountNode(String data, int frequency) {
+            this.data = data;
+            this.frequency = frequency;
+        }
+
+        void incFrequency() {
+            ++frequency;
+        }
+
+    }
+
     public Flux<String> words(Flux<String> stream) {
-        Map<String, Integer> wordCountMap = new HashMap<>();
+        Map<String, CountNode> wordCountMap = new HashMap<>();
+        CountNode head = new CountNode();
+        CountNode tail = new CountNode();
+        head.next = tail;
+        tail.prev = head;
+        wordCountMap.put("", head);
+
         return stream.flatMap(this::scrapeWords)
-                .doOnNext(word -> wordCountMap.compute(
-                        word,
-                        (key, val) -> (val == null) ? 1 : val + 1
-                )) // todo improve readability
-                .thenMany(Flux.fromIterable(wordCountMap.keySet())
-                        .sort(Comparator.comparingInt(wordCountMap::get).reversed()) // todo optimize this and sort/reorder actively
-                        .map(key -> String.format("%s: %s", key, wordCountMap.get(key)))
-                );
+                .doOnNext(word -> {
+                    CountNode countNode = wordCountMap.get(word);
+                    if (countNode == null) {
+                        countNode = new CountNode(word, 0);
+
+                        countNode.prev = tail.prev;
+                        countNode.next = tail;
+
+                        tail.prev.next = countNode;
+                        tail.prev = countNode;
+
+                        wordCountMap.put(word, countNode);
+                    }
+                    countNode.incFrequency();
+
+                    CountNode current = countNode.prev;
+                    while (current != head && countNode.frequency > current.frequency) {
+                        current = current.prev;
+                    }
+
+                    if (current != countNode) {
+                        countNode.prev.next = countNode.next;
+                        countNode.next.prev = countNode.prev;
+
+                        countNode.next = current.next;
+                        countNode.prev = current;
+
+                        current.next.prev = countNode;
+                        current.next = countNode;
+                    }
+                })
+                .thenMany(Flux.fromStream(() -> {
+                    CountNode current = head.next;
+                    List<String> values = new ArrayList<>();
+                    while (current != tail) {
+                        values.add(String.format("%s: %s", current.data, current.frequency));
+                        current = current.next;
+                    }
+                    return values.stream();
+                }));
     }
 
     private Flux<String> scrapeWords(String s) {
