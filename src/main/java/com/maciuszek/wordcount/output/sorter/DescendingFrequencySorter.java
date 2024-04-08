88f2d03ceb7955517264cf047eb0414f79703403
maciuszek <szczecinska.duma@gmail.com>
2024-04-08 13:28:08 -0400

    improve readme improve code comments

--- src/main/java/com/maciuszek/wordcount/output/sorter/DescendingFrequencySorter.java
+++ src/main/java/com/maciuszek/wordcount/output/sorter/DescendingFrequencySorter.java
@@ -12,15 +12,19 @@ public class DescendingFrequencySorter implements Sorter<Flux<WordCount>> {
 
     @Override
     public Flux<WordCount> sort(Flux<WordCount> flux) {
+        // for production, redundantly a database would need to be utilized again in place of the hashmap as with com.maciuszek.wordcount.service.BasicCountService,
+        // since the stream size and implicitly the memory of the hashmap is undefined. a better solution would be active sorting as attempted in
+        // https://github.com/maciuszek/word-count-ta/blob/active_sort/src/main/java/com/maciuszek/wordcount/service/ActiveSortingCountService.java
+
         Map<Integer, List<WordCount>> wordCountFequencyMap = new HashMap<>(); // use a hashmap to group the WordCount objects in the flux by frequency
+
         return flux.doOnNext(wordCount -> {
             Integer key = wordCount.getCount();
             List<WordCount> wordCountList = wordCountFequencyMap.getOrDefault(key, new ArrayList<>());
             wordCountList.add(wordCount);
             wordCountFequencyMap.put(key, wordCountList);
-        }) // for production the sorting algorithm would need to leverage a database that can automatically scale since this assumes an undefined amount of memory. a better solution would be active sorting as attempted with https://github.com/maciuszek/word-count-ta/blob/active_sort/src/main/java/com/maciuszek/wordcount/service/ActiveSortingCountService.java
-        .thenMany(Flux.fromStream(() -> {
-            // using the populated hashmap return a new stream of sorted WordCount objects by frequency
+        }).thenMany(Flux.fromStream(() -> {
+            // once all the data has been consolidated into a hashmap, derive a new flux stream of WordCount objects sorted by frequency
             List<Integer> frequencies = new ArrayList<>(wordCountFequencyMap.keySet());
             frequencies.sort(Collections.reverseOrder()); // sort the list of frequencies in descending order
 
